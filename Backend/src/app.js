7008d14c2da6d00d1284df51a478f579eeff6733
Jessé Melara <jmelara9@gmail.com>
2021-06-02 17:36:42 -0600

    Instalacion de babel para usar ES6

---
+++ Backend/src/app.js
@@ -0,0 +1,19 @@
+//Cargar modulos de node para crear servidor
+import express from 'express';
+import { urlencoded, json } from 'body-parser';
+
+//Ejecutar express (http)
+var app = express();
+
+//Cargar ficheros rutas
+
+//Middlewares
+app.use(urlencoded({extended:false}));
+app.use(json());
+
+//CORS
+
+//Añadir prefijos a rutas
+
+//Exportar modulo (fichero actual)
+export default app;
