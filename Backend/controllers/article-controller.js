ab5277020916be5929087b385071343e1edeb3c8
Jessé Melara <jmelara9@gmail.com>
2021-06-02 22:35:37 -0600

    Creacion del controlador article-controller.js

---
+++ Backend/controllers/article-controller.js
@@ -0,0 +1,20 @@
+let controller = {
+
+    datosCurso: (req, res) => {
+        let hola = req.body.hola;
+
+        return res.status(200).send({
+            curso: 'Master en Framewors de JS',
+            author: 'Jesse Melara',
+            hola
+        });
+    },
+
+    test: (req, res) => {
+        return res.status(200).send({
+            message: 'Soy la accion test de mi controlador de articulos'
+        });
+    }
+}; //end controller
+
+export default controller;
