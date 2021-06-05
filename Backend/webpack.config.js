4d30272f84cb4a902674ba8bc451681200001011
Jessé Melara <jmelara9@gmail.com>
2021-06-05 11:51:20 -0600

    Configurando webpack con babel

--- Backend/webpack.config.js
+++ Backend/webpack.config.js
@@ -6,4 +6,18 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, '/'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
