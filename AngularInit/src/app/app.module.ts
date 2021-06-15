f96f65cb8bb54c7f807bee46d9287cc8cc8f4da4
Jessé Melara <jmelara9@gmail.com>
2021-06-15 14:53:09 -0600

    Creacion del Componente Sidebar

--- AngularInit/src/app/app.module.ts
+++ AngularInit/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SliderComponent } from './components/slider/slider.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SliderComponent
+    SliderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
