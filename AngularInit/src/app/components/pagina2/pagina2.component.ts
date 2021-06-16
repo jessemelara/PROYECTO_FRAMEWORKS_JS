005e6c79be86fe333b0f5132001daaab0283ddc9
Jessé Melara <jmelara9@gmail.com>
2021-06-15 23:22:46 -0600

    Creacion de contenido en Pagina2 (*ngIF, *ngFor)

--- AngularInit/src/app/components/pagina2/pagina2.component.ts
+++ AngularInit/src/app/components/pagina2/pagina2.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagina2.component.css']
 })
 export class Pagina2Component implements OnInit {
+  public frutas!: Array<any>;
 
-  constructor() { }
-
+  constructor() {
+    this.frutas = [
+      {title: "Apples", image: "https://www.osfhealthcare.org/blog/wp-content/uploads/2019/08/apples-OG.jpg"},
+      {title: "Strawberries", image: "https://www.besthealthmag.ca/wp-content/uploads/2020/08/mushy-strawberries.jpg"},
+      {title: "Watermelon", image: "https://d.newsweek.com/en/full/1812007/watermelon-slices.jpg"},
+      {title: "Cherries", image: "https://extension.usu.edu/preserve-the-harvest/images/cherries.jpg"}
+    ]
+  }
   ngOnInit(): void {
+    console.log(this.frutas);
   }
 
 }
