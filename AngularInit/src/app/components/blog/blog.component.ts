64236ce60ee7510f0f3f25dda86e4cc502a88b42
Jessé Melara <jmelara9@gmail.com>
2021-06-24 13:08:29 -0600

    Mostrar listado de articulos en el Blog

--- AngularInit/src/app/components/blog/blog.component.ts
+++ AngularInit/src/app/components/blog/blog.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from 'src/app/models/article';
 import { ArticleService } from 'src/app/services/article.service';
+import { Global } from 'src/app/services/global';
 
 @Component({
   selector: 'app-blog',
@@ -9,14 +11,21 @@ import { ArticleService } from 'src/app/services/article.service';
 })
 export class BlogComponent implements OnInit {
   blogText = "Blog";
+  public articles!: Article[];
+  public url!:string;
 
   constructor(
     private articleService: ArticleService
-  ) { }
+  ) {
+    this.url = Global.url;
+  }
 
   ngOnInit(): void {
     this.articleService.getArticles().subscribe(
       response => {
+        if(response.articles){
+          this.articles = response.articles;
+        };
         console.log(response);
       },
       error => {
