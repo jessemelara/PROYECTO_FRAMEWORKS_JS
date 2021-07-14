f199fca82bf0d84dd7874d17e9e6122ee85a14e8
Jessé Melara <jmelara9@gmail.com>
2021-07-14 16:33:58 -0600

    Creacion del componente Slider

--- react_init/src/App.js
+++ react_init/src/App.js
@@ -3,11 +3,13 @@ import "./assets/css/App.css";
 
 //Componentes
 import Header from "./components/Header";
+import Slider from "./components/Slider";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Slider />
 
       <header className="App-header"></header>
     </div>
---
+++ react_init/src/components/Slider.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Slider extends Component {
+  render() {
+    return (
+      <div id="slider" class="slider-big">
+        <h1>Bienvenido al Master en ReactJS</h1>
+        <a href="blog.html" class="btn-white">
+          Ir al blog
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Slider;
