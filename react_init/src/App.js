1112e9501c90811b0bc66153e2ee6dda83c78930
Jessé Melara <jmelara9@gmail.com>
2021-07-14 15:47:51 -0600

    Preparando entorno de React

--- react_init/src/App.js
+++ react_init/src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
 
 function App() {
+  var nombre = "Jesse Melara";
+  var presentacion = <h2>Hola, soy {nombre}</h2>;
+  
+  let receta = {
+    nombre: "Pizza",
+    ingredientes: ["Harina", "Tomate", "Queso", "Jamon", "Pepperoni"],
+    calorias: 400
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bienvenido al curso de React
+          Bienvenido al curso de React<br/>
+          {presentacion}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <React.Fragment>
+          <h1>{'Receta: '+receta.nombre}</h1>
+          <h2>{'Calorias: '+receta.calorias}</h2>
+          <ol>
+            {receta.ingredientes.map((ingrediente, i) => {
+              return(
+                <li key={i}>{ingrediente}</li>
+              );
+            })}
+          </ol>
+        </React.Fragment>
       </header>
     </div>
   );
