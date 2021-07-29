67f0c50b7fefa88d1fb30e6856980def10c6caf5
Jessé Melara <jmelara9@gmail.com>
2021-07-28 21:36:35 -0600

    Configuracion de alertas al Crear Articulo

--- react_init/src/components/ArticleCreate.js
+++ react_init/src/components/ArticleCreate.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import SimpleReactValidator from "simple-react-validator";
 import axios from "axios";
+import Swal from "sweetalert2";
 import Global from "../Global";
 import Sidebar from "./Sidebar";
 
@@ -22,8 +23,7 @@ export default class ArticleCreate extends Component {
     this.validator = new SimpleReactValidator({
       autoForceUpdate: this,
       messages: {
-        alpha_space: "Este campo solo permite letras y espacios.",
-        alpha_num_space: "Este campo solo permite letras, números y espacios.",
+        regex: "Este campo solo admite letras, acentos y espacios.",
         required: "Este campo es requerido.",
       },
     });
@@ -51,66 +51,83 @@ export default class ArticleCreate extends Component {
     // Rellenar state con formulario
     this.changeState();
     if (this.validator.allValid()) {
-      // Hacer una peticion HTTP por POST para guardar el articulo
-      axios.post(this.url + "save", this.state.article).then((res) => {
-        console.log(res.data);
+      // Alerta para Guardar articulo
+      Swal.fire({
+        title: "¿Quieres guardar el nuevo artículo creado?",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: `Guardar`,
+        denyButtonText: `No Guardar`,
+        cancelButtonText: `Cancelar`,
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          Swal.fire("Nuevo artículo creado", "", "success");
+          // Hacer una peticion HTTP por POST para guardar el articulo
+          axios.post(this.url + "save", this.state.article).then((res) => {
+            console.log(res.data);
 
-        if (res.data.article) {
-          this.setState({
-            article: res.data.article,
-            status: "waiting",
-          });
-
-          //Subir imagen
-          if (this.state.selectedFile !== null) {
-            //Sacar el id del articulo guardado
-            let articleId = this.state.article._id;
-
-            //Crear form data y agregar el fichero
-            const formData = new FormData();
-            formData.append(
-              "file0",
-              this.state.selectedFile,
-              this.state.selectedFile.name
-            );
-
-            //Peticion AJAX
-            axios
-              .post(this.url + "upload-image/" + articleId, formData)
-              .then((res) => {
-                console.log(res.data);
-                if (res.data.article.image) {
-                  this.setState({
-                    article: res.data.article,
-                    status: "success",
-                  });
-                } else {
-                  this.setState({
-                    article: res.data.article,
-                    status: "success",
-                  });
-                }
-              })
-              .catch((err) => {
-                if (err.response) {
-                  console.log(err.response.data);
-                  console.log(err.response.status);
-                } else if (err.request) {
-                  console.log(err.request);
-                } else {
-                  console.log("Error", err.message);
-                }
-                console.log(err.config);
+            if (res.data.article) {
+              this.setState({
+                article: res.data.article,
+                status: "waiting",
               });
-          } else {
-            this.setState({
-              status: "success",
-            });
-          }
-        } else {
-          this.setState({
-            status: "error",
+
+              //Subir imagen
+              if (this.state.selectedFile !== null) {
+                //Sacar el id del articulo guardado
+                let articleId = this.state.article._id;
+
+                //Crear form data y agregar el fichero
+                const formData = new FormData();
+                formData.append(
+                  "file0",
+                  this.state.selectedFile,
+                  this.state.selectedFile.name
+                );
+
+                //Peticion AJAX
+                axios
+                  .post(this.url + "upload-image/" + articleId, formData)
+                  .then((res) => {
+                    console.log(res.data);
+                    if (res.data.article.image) {
+                      this.setState({
+                        article: res.data.article,
+                        status: "success",
+                      });
+                    } else {
+                      this.setState({
+                        article: res.data.article,
+                        status: "success",
+                      });
+                    }
+                  })
+                  .catch((err) => {
+                    if (err.response) {
+                      console.log(err.response.data);
+                      console.log(err.response.status);
+                    } else if (err.request) {
+                      console.log(err.request);
+                    } else {
+                      console.log("Error", err.message);
+                    }
+                    console.log(err.config);
+                  });
+              } else {
+                this.setState({
+                  status: "success",
+                });
+              }
+            } else {
+              this.setState({
+                status: "error",
+              });
+            }
           });
+        } else if (result.isDenied) {
+          Swal.fire("El artículo no fue creado", "", "info");
+          return <Redirect to={"/blog/create"} />;
         }
       });
     } else {
@@ -139,14 +156,14 @@ export default class ArticleCreate extends Component {
                 name="title"
                 ref={this.titleRef}
                 onChange={this.changeState}
-                onBlur={() => this.validator.showMessageFor("alpha_space")}
+                onBlur={() => this.validator.showMessageFor("regex")}
               />
 
               {/*** Validación de campo ***/}
               {this.validator.message(
                 "title",
                 this.state.article.title,
-                "required|alpha_space",
+                "required|regex:^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\u0020*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$",
                 { className: "text-danger" }
               )}
             </div>
@@ -157,14 +174,14 @@ export default class ArticleCreate extends Component {
                 name="content"
                 ref={this.contentRef}
                 onChange={this.changeState}
-                onBlur={() => this.validator.showMessageFor("alpha_num_space")}
+                onBlur={() => this.validator.showMessageFor("required")}
               ></textarea>
 
               {/*** Validación de campo ***/}
               {this.validator.message(
                 "content",
                 this.state.article.content,
-                "required|alpha_num_space",
+                "required",
                 { className: "text-danger" }
               )}
             </div>
