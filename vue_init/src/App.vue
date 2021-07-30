9f54f801cc090edbdcf93ada7632340366441fc3
Jessé Melara <jmelara9@gmail.com>
2021-07-30 12:25:17 -0600

    Creacion del componente Sidebar

--- vue_init/src/App.vue
+++ vue_init/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <Header />
   <Slider />
+  <Sidebar />
 </template>
 
 <script>
 import Header from './components/Header.vue'
 import Slider from './components/Slider.vue'
+import Sidebar from './components/Sidebar.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Slider
+    Slider,
+    Sidebar
   }
 }
 </script>
