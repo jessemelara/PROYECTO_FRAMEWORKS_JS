bdb38eaeca9a28e3c3bd668262838ca02147c832
Jessé Melara <jmelara9@gmail.com>
2021-07-30 12:14:21 -0600

    Creacion del componente Header

--- vue_init/src/App.vue
+++ vue_init/src/App.vue
@@ -1,15 +1,18 @@
 <template>
+  <Header />
   <img alt="Vue logo" src="./assets/images/logo.png">
   <HelloWorld msg="Bienvenido al Master en Frameworks de JS"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
 }
 </script>
---
+++ vue_init/src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header id="header">
+            <div class="center">    
+                <!--Logo-->
+                <div id="logo">
+                    <img src="../assets/images/vue.svg" class="app-logo" alt="Logotipo">
+                    <span id="brand">
+                        <strong>Master</strong> VueJS
+                    </span>
+                </div>
+                <!--Menu-->
+                <nav id="menu">
+                    <ul>
+                        <li><a href="index.html">Inicio</a></li>
+                        <li><a href="blog.html">Blog</a></li>
+                        <li><a href="formulario.html">Formulario</a></li>
+                        <li><a href="#">Pagina 1</a></li>
+                        <li><a href="#">Pagina 2</a></li>
+                    </ul>
+                </nav>
+
+                <!--Limpiar flotados-->
+                <div class="clearfix"></div>
+            </div>
+        </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
