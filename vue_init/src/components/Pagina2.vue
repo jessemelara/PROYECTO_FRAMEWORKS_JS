73aad10d862085d9be490785da1f0b0bc0a0e816
Jessé Melara <jmelara9@gmail.com>
2021-08-03 03:18:53 -0600

    Creacion de contenido en Pagina 2

--- vue_init/src/components/Pagina2.vue
+++ vue_init/src/components/Pagina2.vue
@@ -1,10 +1,60 @@
 <template>
-  <h1 class="subheader">Página 2</h1>
+  <h1 class="subheader">Animes</h1>
+  <div id="articles">
+    <article
+      class="article-item"
+      id="article-template"
+      v-for="anime in animes"
+      v-bind:key="anime.title"
+    >
+      <div class="image-wrap">
+        <img :src="anime.image" :alt="anime.title" />
+      </div>
+      <h2>{{ anime.title }}</h2>
+      <span class="date">
+        Hace 5 minutos
+      </span>
+      <a href="article.html">Leer más</a>
+
+      <div class="clearfix"></div>
+    </article>
+  </div>
 </template>
 
 ;
 <script>
 export default {
   name: "Pagina2",
+  data() {
+    return {
+      animes: [
+        {
+          title: "Boku No Hero",
+          image:
+            "https://a-static.besthdwallpaper.com/my-hero-academia-izuku-midoriya-one-for-all-full-cowling-wallpaper-16253_L.jpg",
+        },
+        {
+          title: "Kimetsu No Yaiba",
+          image:
+            "https://a-static.besthdwallpaper.com/demon-slayer-kamado-tanjirou-wallpaper-25601_L.jpg",
+        },
+        {
+          title: "Nanatzu No Taizai",
+          image:
+            "https://i.pinimg.com/originals/f0/f2/66/f0f266b07f49f8365467e6a6a9d07cd3.jpg",
+        },
+        {
+          title: "Dragon Ball Super",
+          image:
+            "https://a-static.besthdwallpaper.com/dragon-ball-super-broly-movie-vegeta-super-saiyan-blue-wallpaper-20245_L.jpg",
+        },
+        {
+          title: "Avatar: The Legend of Aang",
+          image:
+            "https://1.bp.blogspot.com/-DkmgAUwRr2A/Xx9jqgMvEmI/AAAAAAABV0g/a1sx7_DUrCYKVvGY7zE2eJYLJIfQxiXLwCNcBGAsYHQ/s1600/avatar_sequel_comic-publicity_-_h_2020_.jpg",
+        },
+      ],
+    };
+  },
 };
 </script>
