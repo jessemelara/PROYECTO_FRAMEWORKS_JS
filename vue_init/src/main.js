cf945a8d8413b1bd3327a818414271591754b3cc
Jessé Melara <jmelara9@gmail.com>
2021-08-02 22:53:08 -0600

    Creacion de pagina de Error 404

--- vue_init/src/main.js
+++ vue_init/src/main.js
@@ -4,6 +4,7 @@ import Blog from "./components/Blog.vue";
 import Formulario from "./components/Formulario.vue";
 import Pagina1 from "./components/Pagina1.vue";
 import Pagina2 from "./components/Pagina2.vue";
+import NotFound from "./components/NotFound.vue";
 
 //Paquetes
 import { createApp } from "vue";
@@ -17,6 +18,7 @@ const routes = [
   { path: "/formulario", component: Formulario },
   { path: "/pagina1/:nombre?/:apellido?", name: "pagina1", component: Pagina1 },
   { path: "/pagina2", component: Pagina2 },
+  { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
 ];
 
 const router = createRouter({
