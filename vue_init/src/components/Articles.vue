0c0a4e5209a14cc6c8beeef630f51783f3aa6499
Jessé Melara <jmelara9@gmail.com>
2021-08-06 02:21:57 -0600

    Creacion del componente Articles

---
+++ vue_init/src/components/Articles.vue
@@ -0,0 +1,42 @@
+<template>
+  <article
+    class="article-item"
+    id="article-template"
+    v-for="article in articles"
+    :key="article._id"
+  >
+    <div class="image-wrap">
+      <img
+        :src="url + 'get-image/' + article.image"
+        :alt="article.title"
+        v-if="article.image"
+      />
+      <img
+        src="../assets/images/default-image.svg"
+        :alt="article.title"
+        v-else
+      />
+    </div>
+    <h2>{{ article.title }}</h2>
+    <span class="date">
+      Hace 5 minutos
+    </span>
+    <a href="article.html">Leer más</a>
+
+    <div class="clearfix"></div>
+  </article>
+</template>
+
+<script>
+import Global from "../Global";
+
+export default {
+  name: "Articles",
+  props: ["articles"],
+  data() {
+    return {
+      url: Global.url,
+    };
+  },
+};
+</script>
--- vue_init/src/components/Blog.vue
+++ vue_init/src/components/Blog.vue
@@ -3,33 +3,7 @@
   <div class="center">
     <section id="content">
       <h2 class="subheader">Blog</h2>
-
-      <article
-        class="article-item"
-        id="article-template"
-        v-for="article in articles"
-        :key="article._id"
-      >
-        <div class="image-wrap">
-          <img
-            :src="url + 'get-image/' + article.image"
-            :alt="article.title"
-            v-if="article.image"
-          />
-          <img
-            src="../assets/images/default-image.svg"
-            :alt="article.title"
-            v-else
-          />
-        </div>
-        <h2>{{ article.title }}</h2>
-        <span class="date">
-          Hace 5 minutos
-        </span>
-        <a href="article.html">Leer más</a>
-
-        <div class="clearfix"></div>
-      </article>
+      <Articles :articles="articles" />
     </section>
     <Sidebar />
     <div class="clearfix"></div>
@@ -39,6 +13,7 @@
 <script>
 import Slider from "./Slider.vue";
 import Sidebar from "./Sidebar.vue";
+import Articles from "./Articles.vue";
 import Global from "../Global";
 
 import axios from "axios";
@@ -48,15 +23,16 @@ export default {
   components: {
     Slider,
     Sidebar,
-  },
-  mounted() {
-    this.getArticle();
+    Articles,
   },
   data() {
     return {
       url: Global.url,
       articles: [],
     };
+  },
+  mounted() {
+    this.getArticle();
   },
   methods: {
     getArticle() {
