e3d7da28ff0811dbbf8c88f2ebe4b6f3a144d4c2
Jessé Melara <jmelara9@gmail.com>
2021-08-06 04:31:24 -0600

    Metodo para mostrar Ultimos Articulos

--- vue_init/src/components/Home.vue
+++ vue_init/src/components/Home.vue
@@ -3,6 +3,7 @@
   <div class="center">
     <section id="content">
       <h2 class="subheader">Últimos artículos</h2>
+      <Articles v-bind:articles="articles" />
     </section>
     <Sidebar />
     <div class="clearfix"></div>
@@ -12,12 +13,37 @@
 <script>
 import Slider from "./Slider.vue";
 import Sidebar from "./Sidebar.vue";
+import Articles from "./Articles.vue";
+import Global from "../Global";
+
+import axios from "axios";
 
 export default {
   name: "Home",
   components: {
     Slider,
     Sidebar,
+    Articles,
+  },
+  data() {
+    return {
+      url: Global.url,
+      articles: [],
+    };
+  },
+  mounted() {
+    this.getLastArticle();
+  },
+  methods: {
+    getLastArticle() {
+      axios.get(Global.url + "articles/last").then((res) => {
+        if (res.data.status == "success") {
+          this.articles = res.data.articles;
+        }
+
+        console.log(res.data);
+      });
+    },
   },
 };
 </script>
