8f04026518fbd8edbe237ed3f2cb9987140d64ca
Jessé Melara <jmelara9@gmail.com>
2021-08-06 01:36:07 -0600

    Metodo para Mostrar articulos en el Blog

---
+++ vue_init/src/Global.js
@@ -0,0 +1,5 @@
+const Global = {
+  url: "http://localhost:3900/api/",
+};
+
+export default Global;
--- vue_init/src/components/Blog.vue
+++ vue_init/src/components/Blog.vue
@@ -3,6 +3,33 @@
   <div class="center">
     <section id="content">
       <h2 class="subheader">Blog</h2>
+
+      <article
+        class="article-item"
+        id="article-template"
+        v-for="article in articles"
+        :key="article._id"
+      >
+        <div class="image-wrap">
+          <img
+            :src="url + 'get-image/' + article.image"
+            :alt="article.title"
+            v-if="article.image"
+          />
+          <img
+            src="../assets/images/default-image.svg"
+            :alt="article.title"
+            v-else
+          />
+        </div>
+        <h2>{{ article.title }}</h2>
+        <span class="date">
+          Hace 5 minutos
+        </span>
+        <a href="article.html">Leer más</a>
+
+        <div class="clearfix"></div>
+      </article>
     </section>
     <Sidebar />
     <div class="clearfix"></div>
@@ -12,12 +39,35 @@
 <script>
 import Slider from "./Slider.vue";
 import Sidebar from "./Sidebar.vue";
+import Global from "../Global";
+
+import axios from "axios";
 
 export default {
   name: "Blog",
   components: {
     Slider,
     Sidebar,
+  },
+  mounted() {
+    this.getArticle();
+  },
+  data() {
+    return {
+      url: Global.url,
+      articles: [],
+    };
+  },
+  methods: {
+    getArticle() {
+      axios.get(Global.url + "articles").then((res) => {
+        if (res.data.status == "success") {
+          this.articles = res.data.articles;
+        }
+
+        console.log(res.data);
+      });
+    },
   },
 };
 </script>
