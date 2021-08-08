ddf839cd344f3e5f5f31936e108926106a185c81
Jessé Melara <jmelara9@gmail.com>
2021-08-08 12:10:06 -0600

    Configuracion de alertas al crear articulos

--- vue_init/src/components/ArticleCreate.vue
+++ vue_init/src/components/ArticleCreate.vue
@@ -57,6 +57,7 @@
 import Sidebar from "./Sidebar.vue";
 import Global from "../Global";
 import axios from "axios";
+import Swal from "sweetalert2";
 import { useRouter } from "vue-router";
 import { ref } from "vue";
 import { useValidation, ValidationError } from "vue3-form-validation";
@@ -112,53 +113,68 @@ export default {
         const formData = await validateFields();
         console.log(formData);
 
-        //Guardar datos en el backend
-        axios
-          .post(`${url}save`, formData.article)
-          .then((res) => {
-            console.log(res.data);
-            if (res.data.status == "success") {
-              if (image != null && image != undefined && image != "") {
-                //Subida de imagen
-                const imageData = new FormData();
-                imageData.append("file0", image, image.name);
-                let articleId = res.data.article._id;
+        Swal.fire({
+          title: "¿Quieres guardar este nuevo artículo?",
+          showDenyButton: true,
+          showCancelButton: true,
+          confirmButtonText: `Guardar`,
+          denyButtonText: `No Guardar`,
+          cancelButtonText: `Cancelar`,
+        }).then((result) => {
+          /* Read more about isConfirmed, isDenied below */
+          if (result.isConfirmed) {
+            //Guardar datos en el Backend
+            axios
+              .post(`${url}save`, formData.article)
+              .then((res) => {
+                console.log(res.data);
+                if (res.data.status == "success") {
+                  if (image != null && image != undefined && image != "") {
+                    //Subida de imagen
+                    const imageData = new FormData();
+                    imageData.append("file0", image, image.name);
+                    let articleId = res.data.article._id;
 
-                //Peticion AJAX
-                axios
-                  .post(`${url}upload-image/${articleId}`, imageData)
-                  .then((res) => {
-                    if (res.data.article) {
-                      formData.article = res.data.article;
-                      router.push("/blog");
-                    } else {
-                      //Alertas
-                    }
-                  })
-                  .catch(function(error) {
-                    if (error.response) {
-                      console.log(error.response.data);
-                      console.log(error.response.status);
-                    } else {
-                      console.log("Error", error.message);
-                    }
-                    console.log(error.config);
-                  });
-              } else {
-                formData.article = res.data.article;
-                router.push("/blog");
-              }
-            }
-          })
-          .catch(function(error) {
-            if (error.response) {
-              console.log(error.response.data);
-              console.log(error.response.status);
-            } else {
-              console.log("Error", error.message);
-            }
-            console.log(error.config);
-          }); //End --Articulos guardados--
+                    //Peticion AJAX
+                    axios
+                      .post(`${url}upload-image/${articleId}`, imageData)
+                      .then((res) => {
+                        if (res.data.article) {
+                          formData.article = res.data.article;
+                          router.push("/blog");
+                        }
+                      })
+                      .catch(function(error) {
+                        if (error.response) {
+                          console.log(error.response.data);
+                          console.log(error.response.status);
+                        } else {
+                          console.log("Error", error.message);
+                        }
+                        console.log(error.config);
+                      });
+                  } else {
+                    formData.article = res.data.article;
+                    router.push("/blog");
+                  }
+                }
+              })
+              .catch(function(error) {
+                if (error.response) {
+                  console.log(error.response.data);
+                  console.log(error.response.status);
+                } else {
+                  console.log("Error", error.message);
+                }
+                console.log(error.config);
+              }); //End --Articulos guardados--
+
+            Swal.fire("¡El artículo ha sido guardado!", "", "success");
+          } else if (result.isDenied) {
+            router.push("/blog/create-article");
+            Swal.fire("!El artículo no ha sido creado!", "", "info");
+          }
+        });
       } catch (e) {
         if (e instanceof ValidationError) {
           console.log(e.message);
